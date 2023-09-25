3208644fa44296c7b9656e63fd732104bd414fda
JD Calvelli <jdcalvelli@jds-macbook-air.lan>
2023-09-25 15:39:05 -0400

    adding film grain

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -10,12 +10,20 @@ func _ready():
 func _on_last_dress(dressNum:int):
 	if dressNum == 7:
 		var tween = create_tween()
+		tween.set_parallel(true)
 		tween.tween_property(
 			$PointLight2D, 
 			"texture_scale", 
 			2.2, 
 			aspectRatioTiming
 			)
+		tween.tween_property(
+			$FilmGrain,
+			"modulate",
+			Color(0, 0, 0, 0),
+			aspectRatioTiming / 2
+		)
+		tween.set_parallel(false)
 		tween.tween_interval(
 			waitTime
 			)
